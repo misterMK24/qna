a22f10443ef8e332f5e1079f6b03e48dbc7126ad
mistermk24 <aleksandr.seleznev24@gmail.com>
2020-11-01 14:06:01 +0100

    Auth specs

--- spec/features/authentication/sign_in_spec.rb
+++ spec/features/authentication/sign_in_spec.rb
@@ -9,17 +9,13 @@ feature 'User can sign in', %q{
   given(:user) { create(:user) }
 
   background do
-    # visit login page
     visit new_user_session_path
   end
 
   scenario 'Registered user tries to sign in' do
     fill_in 'Email',	with: user.email
     fill_in 'Password', with: user.password
-    # универсальный хэлпер для нажатия на ссылки или кнопки
     click_on 'Log in'
-    # при помощи гема launchy. открывает страницу перед тестом 
-    # save_and_open_page
 
     expect(page).to have_content 'Signed in successfully.'
   end
@@ -27,7 +23,6 @@ feature 'User can sign in', %q{
   scenario 'Unregistered user tries to sign in' do
     fill_in 'Email',	with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
-    # универсальный хэлпер для нажатия на ссылки или кнопки
     click_on 'Log in'
 
     expect(page).to have_content 'Invalid Email or password.'
