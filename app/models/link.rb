e60d437077c8cbaabae1fb624a181b36c55ef78e
MisterMk24 <aleksandr.seleznev24@gmail.com>
2020-11-24 22:57:26 +0100

    Rendering gist-embeded links

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,16 @@ class Link < ApplicationRecord
 
   validates :name, :url, presence: true
   validates :url, url: true
+
+  def is_gist?
+    GIST_TEMPLATE.match?(self.url)
+  end
+
+  def gist_id
+    GIST_TEMPLATE.match(self.url)[2]
+  end
+
+  private
+
+  GIST_TEMPLATE = /gist\.github\.com\/(\w*)\/(\w*)/
 end
