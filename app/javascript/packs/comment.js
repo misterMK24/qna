d80114e69f0ee75bcc9c1acb05ec9e789d93d146
MisterMk24 <Aleksandr.seleznev24@gmail.com>
2021-04-04 16:07:30 +0200

    add comments for answer

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -1,20 +1,34 @@
 document.addEventListener('turbolinks:load', function() {
   let newQuestionCommentElement = document.querySelector('.btn_new_question_comment')
-  let questionComments = document.querySelectorAll('a.delete_question_comment')
+  let Comments = document.querySelectorAll('a.delete_comment')
 
-  if (newQuestionCommentElement) { newQuestionCommentElement.addEventListener('click', newCommentForm) }
-  questionComments.forEach(comment => { comment.addEventListener('click', deleteQuestionComment) })
+  let newAnswerCommentElements = document.querySelectorAll('.btn_new_answer_comment')
+
+
+  newAnswerCommentElements.forEach(new_comment => { new_comment.addEventListener('click', newAnswerCommentForm) })
+  if (newQuestionCommentElement) { newQuestionCommentElement.addEventListener('click', newQuestionCommentForm) }
+  Comments.forEach(comment => { comment.addEventListener('click', deleteComment) })
 })
 
-function newCommentForm(e) {
+function newQuestionCommentForm(e) {
   e.preventDefault()
-  var editForm = document.querySelector('form#new_comment_question')
+  let questionId = this.dataset.questionId
+  var editForm = document.querySelector('form#new_comment_question' + '_' + questionId)
   editForm.classList.remove('d-none')
   this.classList.add('d-none')
-  document.getElementById('comment_body').value = ""
+  editForm.querySelector('textarea').value = ""
 }
 
-function deleteQuestionComment(e) {
+function newAnswerCommentForm(e) {
+  e.preventDefault()
+  let answerId = this.dataset.answerId
+  var editForm = document.querySelector('form#new_comment_answer' + '_' + answerId)
+  editForm.classList.remove('d-none')
+  this.classList.add('d-none')
+  editForm.querySelector('textarea').value = ""
+}
+
+function deleteComment(e) {
   e.preventDefault()
 
   this.parentElement.remove()
